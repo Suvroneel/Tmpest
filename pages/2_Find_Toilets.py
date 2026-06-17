1bbd6922a74bfbd5f0b113714a5c80f0c28e075e
Suvroneel Nathak <suvroneelnathak213@gmail.com>
2026-06-17 01:37:07 -0700

    Delete pages/2_Find_Toilets.py

--- pages/2_Find_Toilets.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from Utils import title
-from Utils.section import section_start
-from Utils.title import render_custom_header,section_divider
-
-render_custom_header("Find Toilets")
-
-
-section_divider()
